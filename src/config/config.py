b8c2c656de86ac49e74ffcaca47230c5de08930b
“utsav <kashyap.utsav2001@gmail.com>
2026-04-02 18:40:55 +0530

    feat: add logging utility and config constants

---
+++ src/config/config.py
@@ -0,0 +1,6 @@
+import os
+
+ARTIFACTS_DIR = "artifacts"
+MODEL_PATH = os.path.join(ARTIFACTS_DIR, "model.pkl")
+THRESHOLD_PATH = os.path.join(ARTIFACTS_DIR, "threshold.pkl")
+METRICS_PATH = os.path.join(ARTIFACTS_DIR, "metrics.json")
